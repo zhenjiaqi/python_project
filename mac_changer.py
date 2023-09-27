c3c1c5ac4fb080aa164ed1302d18d65ea3cbc63d
jq <zhenjiaqi05@gmail.com>
2023-09-27 10:42:41 +0800

    Update mac_changer.py
    fix the typeerror

--- mac_changer.py
+++ mac_changer.py
@@ -28,7 +28,7 @@ def get_arguments():
 
 def get_current_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
-    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
     if mac_address_search_result:
         return mac_address_search_result.group(0)
     else:
